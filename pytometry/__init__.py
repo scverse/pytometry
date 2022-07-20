ae9b21cb4907beabed1f2b99515c272a073acb84
mbuttner <maren.buettner@helmholtz-muenchen.de>
2022-07-20 11:30:05 +0200

    migrate preprocessing

--- pytometry/__init__.py
+++ pytometry/__init__.py
@@ -15,4 +15,5 @@ This is the complete API reference:
 
 __version__ = "0.0.1"  # denote a pre-release for 0.1.0 with 0.1a1
 
+from . import preprocessing as pp
 from ._core import ExampleClass, example_function  # noqa
---
+++ pytometry/preprocessing/__init__.py
@@ -0,0 +1,6 @@
+from ._process_data import (
+    create_spillover_mat, 
+    create_comp_mat, 
+    find_indexes, 
+    compensate, 
+    split_area)
