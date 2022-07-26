a0b98c59c3e542789771b82b231752aa1b554e3a
mbuttner <maren.buettner@helmholtz-muenchen.de>
2022-07-26 16:20:44 +0200

    fix import

--- pytometry/read_write/fileconverter.py
+++ pytometry/read_write/fileconverter.py
@@ -16,8 +16,8 @@ import anndata as ann
 import numpy as np
 import pandas as pd
 
-from pytometry.preprocessing import process_data as proc
-from pytometry.converter import fcswriter
+from ..preprocessing import _process_data
+from . import fcswriter
 
 
 
