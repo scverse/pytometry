4ebe0c3321165ab3d74dbea3c9e4996b080a57c8
mbuttner <maren.buettner@helmholtz-muenchen.de>
2022-08-01 11:46:36 +0200

    :art: use marker as index in read

--- pytometry/read_write/_readfcs.py
+++ pytometry/read_write/_readfcs.py
@@ -19,4 +19,10 @@ def read_fcs(path: str):
     fcsfile = readfcs.FCSFile(path)
     adata = fcsfile.to_anndata()
 
+    # move marker name to index. Then merging data becomes easier
+    if "marker" in adata.var_keys():
+        adata.var["channel"] = adata.var_names.values
+        adata.var_names = adata.var["marker"]
+        del adata.var["marker"]
+
     return adata
