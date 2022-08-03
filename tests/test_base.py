e51ca125eda2154552057ce8074525e062e7c241
mbuttner <maren.buettner@helmholtz-muenchen.de>
2022-08-03 13:28:22 +0200

    :white_check_mark: Test read function

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,4 +1,15 @@
-# from pytometry import ExampleClass, example_function
+import anndata
+import readfcs
+
+from pytometry.read_write import read_fcs
+
+
+def test_read_fcs():
+    from urllib.request import urlretrieve
+
+    path_data, _ = urlretrieve(readfcs.datasets.example(), "example.fcs")
+    adata = read_fcs(path_data)
+    assert isinstance(adata, anndata._core.anndata.AnnData)
 
 
 # def test_dummy():
