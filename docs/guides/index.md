34983717ce51dc6711fb1bbc3f61b851ce2949f1
mbuttner <maren.buettner@helmholtz-muenchen.de>
2022-08-03 13:10:48 +0200

    :memo: update index

--- docs/guides/index.md
+++ docs/guides/index.md
@@ -1,9 +1,9 @@
 # Guides
 
-These are guides on specific problems. The notebooks get executed in CI for testing.
+These are guides on specific problems. The notebooks are executed in CI for testing.
 
 ```{toctree}
 :maxdepth: 1
 
-curate-data
+
 ```
--- pytometry/__init__.py
+++ pytometry/__init__.py
@@ -9,8 +9,9 @@ This is the complete API reference:
 .. autosummary::
    :toctree: .
 
-   example_function
-   ExampleClass
+   io.read_fcs
+   pp.compensate
+
 """
 
 __version__ = "0.0.1"  # denote a pre-release for 0.1.0 with 0.1a1
@@ -18,4 +19,5 @@ __version__ = "0.0.1"  # denote a pre-release for 0.1.0 with 0.1a1
 from . import preprocessing as pp
 from . import read_write as io
 from . import tools as tl
-from ._core import ExampleClass, example_function  # noqa
+
+# from ._core import ExampleClass, example_function  # noqa
