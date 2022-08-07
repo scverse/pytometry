8194bfaa67efdaf297e3dc9c2e6fbf57d6bc4102
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-08-07 21:22:18 +0800

    :white_check_mark: fix tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -5,14 +5,6 @@ from pytometry.read_write import read_fcs
 
 
 def test_read_fcs():
-    from urllib.request import urlretrieve
-
-    path_data, _ = urlretrieve(readfcs.datasets.example(), "example.fcs")
+    path_data = readfcs.datasets.example()
     adata = read_fcs(path_data)
     assert isinstance(adata, anndata._core.anndata.AnnData)
-
-
-# def test_dummy():
-#    assert example_function("A") == "a"
-#    ex = ExampleClass(1)
-#    assert ex.bar() == "hello"
