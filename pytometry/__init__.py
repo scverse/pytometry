606e8f20d2dd94149fbec1cdd181ace0da1b62e0
mbuttner <maren.buettner@helmholtz-muenchen.de>
2022-08-09 16:40:37 +0200

    :art: add plotting to init

--- pytometry/__init__.py
+++ pytometry/__init__.py
@@ -20,8 +20,9 @@ This is the complete API reference:
 
 """
 
-__version__ = "0.0.1"  # denote a pre-release for 0.1.0 with 0.1a1
+__version__ = "0.1a1"  # denote a pre-release for 0.1.0 with 0.1a1
 
+from . import plotting as pl
 from . import preprocessing as pp
 from . import read_write as io
 from . import tools as tl
