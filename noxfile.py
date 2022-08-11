76eadfe13b50a4b1edfee28060a8e9ff3111e759
Alex Wolf <f.alexander.wolf@gmail.com>
2022-08-11 13:13:16 +0200

    :green_heart: Fix noxfile

--- noxfile.py
+++ noxfile.py
@@ -24,6 +24,6 @@ def build(session):
     )
     session.run("coverage", "xml")
     prefix = "." if Path("./lndocs").exists() else ".."
-    session.run("rm", "./lamin_sphinx/_templates/footer.html")
+    Path("{prefix}/lndocs/lamin_sphinx/_templates/footer.html").unlink()
     session.install(f"{prefix}/lndocs")
     session.run("lndocs")
