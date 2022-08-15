16eacc2dfe56047b9d52a0d9e0b0997e422dc3ba
mbuttner <maren.buettner@helmholtz-muenchen.de>
2022-08-15 14:29:08 +0200

    :white_check_mark: Test compensation

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,17 +1,65 @@
 import anndata
+import numpy
+import pandas
 import readfcs
 
+from pytometry.preprocessing import _dummy_spillover, compensate, create_comp_mat
 from pytometry.read_write import read_fcs
 from pytometry.tools import normalize_arcsinh, normalize_biExp, normalize_logicle
 
 
+# test read function
 def test_read_fcs():
     path_data = readfcs.datasets.example()
     adata = read_fcs(path_data)
     assert isinstance(adata, anndata._core.anndata.AnnData)
 
 
-# test return types
+# test compensate
+def test_compensate():
+    path_data = readfcs.datasets.example()
+    adata = read_fcs(path_data)
+    compensate(adata)
+    assert isinstance(adata, anndata._core.anndata.AnnData)
+
+
+def test_compensate_inplace():
+    path_data = readfcs.datasets.example()
+    adata = read_fcs(path_data)
+    adata2 = compensate(adata, inplace=False)
+    assert isinstance(adata2, anndata._core.anndata.AnnData)
+
+
+def test_spill():
+    n_rows = 12
+    spillmat = _dummy_spillover(n_rows=n_rows)
+    assert isinstance(spillmat, pandas.DataFrame)
+    assert spillmat.shape == (n_rows, n_rows)
+
+
+def test_create_comp_mat():
+    n_rows = 5
+    spillmat = _dummy_spillover(n_rows=n_rows)
+    comp_mat = create_comp_mat(spillmat)
+    assert numpy.sum(comp_mat) == n_rows * 0.5
+
+
+# test custom dummy compensation
+def test_compensate2():
+    path_data = readfcs.datasets.example()
+    adata = read_fcs(path_data)
+    # use extra compensation matrix
+    spillmat = _dummy_spillover(
+        n_rows=adata.uns["meta"]["spill"].shape[0],
+        row_names=adata.uns["meta"]["spill"].index,
+    )
+    adata2 = compensate(
+        adata, comp_matrix=spillmat, matrix_type="spillover", inplace=False
+    )
+    assert adata2.X.sum() != adata.X.sum()
+
+
+# test return types for normalization
 def test_normalize_arcsinh():
     path_data = readfcs.datasets.example()
     adata = read_fcs(path_data)
