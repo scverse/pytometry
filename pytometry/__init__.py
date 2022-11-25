3c6900b274b5feea50c50a513b1ad6f84610c396
mbuttner <maren.buettner@helmholtz-muenchen.de>
2022-11-25 11:34:12 +0100

    :memo: add annotation nb

--- pytometry/__init__.py
+++ pytometry/__init__.py
@@ -6,21 +6,37 @@ Import the package::
 
 This is the complete API reference:
 
+.. module pytometry.io
+
 .. autosummary::
    :recursive:
    :toctree: .
 
-   read_write.read_fcs
-   preprocessing.split_signal
-   preprocessing.compensate
-   preprocessing.find_indexes
-   tools.normalize_arcsinh
-   tools.normalize_logicle
-   tools.normalize_biExp
+   io.read_fcs
+
+.. module pytometry.pp
+
+.. autosummary::
+   :recursive:
+   :toctree: .
+
+   pp.split_signal
+   pp.compensate
+   pp.find_indexes
+
+..module pytometry.tl
+
+.. autosummary::
+   :recursive:
+   :toctree: .
+
+   tl.normalize_arcsinh
+   tl.normalize_logicle
+   tl.normalize_biExp
 
 """
 
-__version__ = "0.1.2"  # denote a pre-release for 0.1.0 with 0.1a1
+__version__ = "0.1.3"  # denote a pre-release for 0.1.0 with 0.1a1
 
 from . import plotting as pl
 from . import preprocessing as pp
