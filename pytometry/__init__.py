5b815b5beb0a627737f309413a7e8c234f319b7e
mbuttner <maren.buettner@helmholtz-muenchen.de>
2022-11-25 13:06:21 +0100

    :memo: update notebooks

--- pytometry/__init__.py
+++ pytometry/__init__.py
@@ -1,10 +1,3 @@
-__version__ = "0.1.3"  # denote a pre-release for 0.1.0 with 0.1a1
-
-from . import plotting as pl
-from . import preprocessing as pp
-from . import read_write as io
-from . import tools as tl
-
 """Flow & mass cytometry analytics.
 
 Import the package::
@@ -13,35 +6,23 @@ Import the package::
 
 This is the complete API reference:
 
-.. module:: pytometry.io
-.. currentmodule:: pytometry
-
 .. autosummary::
    :recursive:
    :toctree: .
 
-   io.read_fcs
-
-.. module:: pytometry.pp
-.. currentmodule:: pytometry
-
-.. autosummary::
-   :recursive:
-   :toctree: .
-
-   pp.split_signal
-   pp.compensate
-   pp.find_indexes
-
-..module:: pytometry.tl
-.. currentmodule:: pytometry
-
-.. autosummary::
-   :recursive:
-   :toctree: .
-
-   tl.normalize_arcsinh
-   tl.normalize_logicle
-   tl.normalize_biExp
+   read_write.read_fcs
+   preprocessing.split_signal
+   preprocessing.compensate
+   preprocessing.find_indexes
+   tools.normalize_arcsinh
+   tools.normalize_logicle
+   tools.normalize_biExp
 
 """
+
+__version__ = "0.1.3"  # denote a pre-release for 0.1.0 with 0.1a1
+
+from . import plotting as pl
+from . import preprocessing as pp
+from . import read_write as io
+from . import tools as tl
