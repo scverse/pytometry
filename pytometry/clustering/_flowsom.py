50e5462fd87c431599f63d7a3c29940c7a7aeb29
burtonrj <burtonrossj@gmail.com>
2023-09-26 21:41:08 +0100

    Added docstrings

--- pytometry/clustering/_flowsom.py
+++ pytometry/clustering/_flowsom.py
@@ -23,6 +23,25 @@ def som_clustering(
     neighbourhood_function: str = "gaussian",
     verbose: bool = False,
 ) -> MiniSom:
+    """Train a SOM on the given cytometry data.
+
+    Args:
+        x (numpy.array): cytometry data of shape (n_samples, n_features)
+        som_dim (tuple[int, int], default=(10, 10)): dimensions of the SOM
+        sigma (float, default=1.0): radius of the different neighbourhoods in the SOM
+        learning_rate (float, default=0.5): learning rate of the SOM
+        batch_size (int, default=500): batch size for training the SOM
+        seed (int, default=42): random seed for reproducibility
+        weight_init (str, default="random"): weight initialization method, either
+        "random" or "pca". NOTE: if "pca" is chosen, the input data must be scaled to
+        zero mean and unit variance.
+        neighbourhood_function (str, default="gaussian"): neighbourhood function for
+        the SOM
+        verbose (bool, default=False)
+
+    Returns:
+        MiniSom: trained SOM
+    """
     som = MiniSom(
         x=som_dim[0],
         y=som_dim[1],
@@ -52,6 +71,23 @@ def meta_clustering(
     resample_frac: float = 0.5,
     verbose: bool = False,
 ) -> np.ndarray:
+    """Meta-clustering of SOM nodes using consensus clustering.
+
+    Consensus clustering is implemented using the `consensusclustering` package (see
+    https://github.com/burtonrj/consensusclustering for details).
+
+    Args:
+        som (MiniSom): trained SOM
+        n_features (int): number of features in the cytometry data
+        min_clusters (int, default=2): minimum number of clusters to consider
+        max_clusters (int, default=10): maximum number of clusters to consider
+        n_resamples (int, default=100): number of resamples for consensus clustering
+        resample_frac (float, default=0.5): fraction of samples to resample for
+        verbose (bool, default=False)
+
+    Returns:
+        numpy.array: meta-clustering of SOM nodes
+    """
     clustering_obj = AgglomerativeClustering(affinity="euclidean", linkage="ward")
     cc = ConsensusClustering(
         clustering_obj,
@@ -90,6 +126,36 @@ def flowsom_clustering(
     copy: bool = False,
     verbose: bool = False,
 ) -> AnnData:
+    """Cluster cytometry data using FlowSOM.
+
+    Based on the original FlowSOM R package by Van Gassen et al. (2015)
+    (https://onlinelibrary.wiley.com/doi/full/10.1002/cyto.a.22625.) and the python
+    implementation found at https://github.com/Hatchin/FlowSOM.
+
+    Args:
+        adata (AnnData): annotated data matrix of shape (n_samples, n_features)
+        key_added (str, default="clusters"): key under which to add the cluster labels
+        som_dim (tuple[int, int], default=(10, 10)): dimensions of the SOM
+        sigma (float, default=1.0): radius of the different neighbourhoods in the SOM
+        learning_rate (float, default=0.5): learning rate of the SOM
+        batch_size (int, default=500): batch size for training the SOM
+        seed (int, default=42): random seed for reproducibility
+        weight_init (str, default="random"): weight initialization method, either
+        "random" or "pca". NOTE: if "pca" is chosen, the input data must be scaled to
+        zero mean and unit variance.
+        neighbourhood_function (str, default="gaussian"): neighbourhood function for
+        the SOM
+        min_clusters (int, default=2): minimum number of clusters to consider
+        max_clusters (int, default=10): maximum number of clusters to consider
+        n_resamples (int, default=100): number of resamples for consensus clustering
+        resample_frac (float, default=0.5): fraction of samples to resample for
+        copy (bool, default=False): whether to copy the AnnData object or modify it
+        verbose (bool, default=False)
+
+    Returns:
+        AnnData: annotated data matrix with cluster labels added under `key_added`
+
+    """
     adata = adata.copy() if copy else adata
     logger.info("Running FlowSOM clustering")
     logger.info("Training SOM")
