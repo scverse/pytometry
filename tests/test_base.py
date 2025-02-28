440b4ad62cb78b39d08ce92d3230a56a6ea8d8ce
insavchuk <159912953+insavchuk@users.noreply.github.com>
2025-02-28 19:35:29 +0100

    Bug fix for _normalizeLogicle. Parameter override and return parameter functionality for normalize_logicle. (#83)
    * Fixed bug in _logicleTransform. Added option to use existing params or return params in normalize_autologicle
    
    * Added test for the params_override flag in normalize_autologicle
    
    * Corrected return statement of normalize_autologicle and wrote test_return_params
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * small change to is None
    
    * Fix error in test
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/test_base.py
+++ tests/test_base.py
@@ -2,10 +2,11 @@ import anndata
 import numpy
 import pandas
 import readfcs
+from flowutils import transforms
 
 from pytometry.io import read_fcs
 from pytometry.pp import _dummy_spillover, compensate, create_comp_mat
-from pytometry.tl import normalize_arcsinh, normalize_biexp, normalize_logicle
+from pytometry.tl import normalize_arcsinh, normalize_autologicle, normalize_biexp, normalize_logicle
 
 
 # test read function
@@ -113,3 +114,42 @@ def test_normalize_logicle2():
     adata = read_fcs(path_data)
     adata2 = normalize_logicle(adata, inplace=False)
     assert isinstance(adata2, anndata._core.anndata.AnnData)
+
+
+def test_autologicle_param_override():
+    path_data = readfcs.datasets.Oetjen18_t1()
+    adata = read_fcs(path_data)
+    channel_indices = list(range(adata.n_vars))
+    params = {
+        "w": 0.1,
+        "t": 4000,
+        "m": 4.5,
+        "a": 0,
+    }
+    params_list = [params for _ in range(adata.n_vars)]  # list of identical params
+    result1 = transforms.logicle(adata.X, channel_indices, **params)
+    result2 = normalize_autologicle(adata, inplace=False, params_override=params_list).X
+    assert (result1 == result2).all()
+
+
+def test_return_params():
+    path_data = readfcs.datasets.Oetjen18_t1()
+    adata = read_fcs(path_data)
+    # case 1: non-mutative, return params
+    adata2, params_list = normalize_autologicle(adata, inplace=False, return_params=True)
+    assert isinstance(adata2, anndata._core.anndata.AnnData)
+    assert isinstance(params_list, list)
+    assert isinstance(params_list[0], dict)
+    # case 2: non-mutative, don't return params
+    adata2 = normalize_autologicle(adata, inplace=False, return_params=False)
+    assert isinstance(adata2, anndata._core.anndata.AnnData)
+    # case 3: mutative, return params
+    params_list = normalize_autologicle(adata, inplace=True, return_params=True)
+    assert isinstance(params_list, list)
+    assert isinstance(params_list[0], dict)
+    assert (adata.X == adata2.X).all()  # check if inplace=True works
+    adata = read_fcs(path_data)
+    # case 4: mutative, don't return params
+    result = normalize_autologicle(adata, inplace=True, return_params=False)
+    assert (adata.X == adata2.X).all()  # check if inplace=True works
+    assert result is None
